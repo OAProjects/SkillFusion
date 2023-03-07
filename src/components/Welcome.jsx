b3e5c5529b4df887d2e91d34b49895d90264ecee
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2023-03-07 18:09:23 +0000

    used vite to create react app

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import "../css/Main.css"
+export default function welcome() {
+    return(
+        <>
+        <div className="wrapper">
+        <h1>Welcome</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum harum, vel officiis, sunt voluptates, ipsam sapiente consectetur tenetur repellat fugit eius tempore iste voluptas consequatur ad quidem eos maiores eaque!</p>
+        </div>
+        </>
+    );
+}
